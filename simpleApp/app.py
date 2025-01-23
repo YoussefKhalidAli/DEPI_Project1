53fab17d2c34b2eda736f75e7db7af56d1476f2d
YoussefKhalidAlim <yossofwd@gmail.com>
2025-01-23 12:47:00 +0000

    changed app.py for testing

--- simpleApp/app.py
+++ simpleApp/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps!"
+    return "Hello, DevOps!, 1/23/25 2:47"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
